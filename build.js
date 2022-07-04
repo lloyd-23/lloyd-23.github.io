17e6e1b8d201f74d2c26b7d20453ad233113d4c2
lloyd-23 <70870251+lloyd-23@users.noreply.github.com>
2022-07-04 19:58:56 +0800

    Update build.js
    change nav color to black

--- build.js
+++ build.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#282d32';
+		header.style.backgroundColor = 'black';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
